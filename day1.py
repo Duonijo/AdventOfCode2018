cfe8a621e825a83778a756ff3837fafd2956a917
Duonijo <39103457+Duonijo@users.noreply.github.com>
2018-12-01 14:04:00 +0100

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,6 +1,7 @@
 import itertools
 content = open('day1input1.txt', 'r').read().replace('\n',' ')
 
+#test avec la liste de l'exemple
 list_test = [1, -2, 3, 1]
 
 #PART ONE
@@ -9,8 +10,6 @@ list = content.split()
 for i in range(len(list)):
    sum += int(list[i])
 print(sum)
-
-
 #La réponse à mon input est 505
 
 
@@ -26,3 +25,4 @@ for i in itertools.cycle(list):
         print(frequence);
         break
     list_part_two.add(frequence)
+#La réponse à mon input est 72330
