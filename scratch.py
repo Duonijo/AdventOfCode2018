d9398daffce688f923193dd5f6890e48145fa7dd
Duonijo <39103457+Duonijo@users.noreply.github.com>
2018-12-01 14:02:10 +0100

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,28 @@
+import itertools
+content = open('day1input1.txt', 'r').read().replace('\n',' ')
+
+list_test = [1, -2, 3, 1]
+
+#PART ONE
+sum = 0
+list = content.split()
+for i in range(len(list)):
+   sum += int(list[i])
+print(sum)
+
+
+#La réponse à mon input est 505
+
+
+#PART TWO
+
+frequence = 0
+list_part_two = set([])
+
+
+for i in itertools.cycle(list):
+    frequence += int(i)
+    if frequence in list_part_two:
+        print(frequence);
+        break
+    list_part_two.add(frequence)
